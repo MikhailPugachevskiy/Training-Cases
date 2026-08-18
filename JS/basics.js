89860d6501fe8cca06a06858f9fc3aafb1419f9e
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-18 21:12:51 +0200

    solved basics js

---
+++ JS/basics.js
@@ -0,0 +1,8 @@
+// Define a variable called myLuckyNumber and set it equal to 37
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+//Define a constant called boilingPointC and set it to 100. Define a second constant called boilingPointF and set it to 212.
+const boilingPointC = 100;
+const boilingPointF = 212;
+
