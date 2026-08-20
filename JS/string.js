8cb3ccc700712429fb49feaa2ba6df1fcb4a0ac7
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-20 13:55:37 +0200

    update string exercise

--- JS/string.js
+++ JS/string.js
@@ -8,3 +8,9 @@ const bestColor = "purple";
 const quote = 'You had me at "hello"';
 
 
+/*provided you with a variable in index.js called message.  Without altering the original value stored in message, 
+please define a variable called whisper that is a lowercased version of message, with all the extra space at the beginning and end removed.  
+You will need to use two string methods that we just covered.  Don't forget you can chain them together on a single line!*/
+
+const message = "    TASTE THE RAINBOW!  ";
+const whisper = message.trim().toLowerCase();
