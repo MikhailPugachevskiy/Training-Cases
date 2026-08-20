8105e1f5b320d23c3e6c0e4120d582a70e394a71
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-20 17:46:20 +0200

    update string exercise

--- JS/string.js
+++ JS/string.js
@@ -8,9 +8,29 @@ const bestColor = "purple";
 const quote = 'You had me at "hello"';
 
 
+
 /*provided you with a variable in index.js called message.  Without altering the original value stored in message, 
+
 please define a variable called whisper that is a lowercased version of message, with all the extra space at the beginning and end removed.  
+
 You will need to use two string methods that we just covered.  Don't forget you can chain them together on a single line!*/
 
 const message = "    TASTE THE RAINBOW!  ";
 const whisper = message.trim().toLowerCase();
+
+
+
+/*provided you with a word variable, set to "skateboard".  Your goal is to use string methods on word, so that you end up with the string "beard". 
+
+    Use a string method to extract the "board" part of "skateboard"  (using the slice method)
+
+    Replace the "o" in board with an "e" (using the replace method)
+
+    Save the result in a variable called facialHair*/
+
+const word = "skateboard"; //Don't change this line!
+const facialHair = word.slice(5, 10).replace('o', 'e');
+
+
+
+
