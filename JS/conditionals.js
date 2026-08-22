1daae4b52832b91f72764e2ce76cd53979e7b5d2
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-22 17:34:10 +0200

    update conditionals exercise js

--- JS/conditionals.js
+++ JS/conditionals.js
@@ -40,3 +40,24 @@ function getColor(phrase) {
     }
     //AND THIS LINE ↑↑↑↑↑
 }
+
+
+
+/* Given you a nested conditional that uses a variable called num. Change the value of num to another number, 
+so that "YOU GOT ME!" is printed out. Don't change anything other than the value of num! Leave the conditional alone!*/
+
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :) 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if (num <= 100) {
+    if (num >= 50) {
+        console.log("HEY!");
+    }
+} else {
+    if (num < 103) {
+        if (num % 2 === 0) {
+            console.log("YOU GOT ME!");
+        }
+    }
+}
