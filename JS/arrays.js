22821a96647cd288771514e2504cbebd0a56637d
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-23 15:32:43 +0200

    finished logicaloperators exercise + begin arrays exercise js

---
+++ JS/arrays.js
@@ -0,0 +1,9 @@
+Lotto Numbers Exercise
+
+//Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  
+
+// Define lottoNumbers below:
+const lottoNumbers = [1, 2, 3, 4, 5, 6];
+
+
+
--- JS/logicaloperators.js
+++ JS/logicaloperators.js
@@ -1,4 +1,49 @@
-/*provided you with a conditional statement that uses a variable called mystery. 
+// =====================
+// LOGICAL AND OPERATOR
+// =====================
+const password = prompt("Enter your password");
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log("VALID PASSWORD!")
+} else {
+    console.log("INCORRECT FORMAT FOR PASSWORD!")
+}
+
+// =====================
+// REVISITING AGE EXAMPLE
+// =====================
+
+// 0-5 free 
+// 5-10 $10 
+// 10-65 $20
+// 65+ free
+
+const age = 100;
+if ((age >= 0 && age < 5) || age >= 65) {
+    console.log("FREE");
+} else if (age >= 5 && age < 10) {
+    console.log("$10")
+} else if (age >= 10 && age < 65) {
+    console.log("$20")
+} else {
+    console.log("INVALID AGE!")
+}
+
+// let firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN!!!");
+// }
+
+// =====================
+// COMBINING && and ||
+// =====================
+const age = 8;
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("YOU ARE NOT A BABY OR A SENIOR!")
+}
+
+
+
+/*Provided you with a conditional statement that uses a variable called mystery. 
 Please change the value of mystery so that the conditional is true and "YOU GOT IT" is printed to the console.  
 Do not change anything other than the value of mystery on line 1!*/
 
@@ -8,3 +53,4 @@ const mystery = 'PS7CHO'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
 if (mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1) {
     console.log("YOU GOT IT!!!");
 }
+
