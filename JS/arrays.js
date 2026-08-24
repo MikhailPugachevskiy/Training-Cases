86a83ee0d72e5d528d01405f6757a84a090c2f34
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-24 22:50:24 +0200

    completed arrays exercise js

--- JS/arrays.js
+++ JS/arrays.js
@@ -26,7 +26,8 @@ leaderboard[3] = 'Draco';
 
 /*Push/Pop/Shift/Unshift Practice
 
-I've provided you with an array called planets. Unfortunately, I'm an idiot who doesn't know much about our solar system and I made some mistakes.  Please help me fix the planets array!  Using the array methods we just learned...
+I've provided you with an array called planets. Unfortunately, I'm an idiot who doesn't know much about our solar system and I made some mistakes.  
+Please help me fix the planets array!  Using the array methods we just learned...
 
     Remove the first element, "The Moon", from the planets array.  The moon is not a planet!
 
@@ -45,3 +46,20 @@ planets.unshift('Mercury')
 
 
 
+/*Nested Arrays Exercise
+
+I've provided you with an array called airplaneSeats, which represents the rows of seats on a flight.  
+Our friend Hugo is looking to book a seat on our flight. Fortunately, there is one middle seat available (it's set to null currently).  
+Please update the array element that is currently null, to instead be "Hugo". */
+
+// DO NOT TOUCH!!! 
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+airplaneSeats[3][1] = 'Hugo';
