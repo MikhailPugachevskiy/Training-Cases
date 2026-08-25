c345fc9611046065f6197daf6fc82ad9e03d1c1d
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-25 23:39:20 +0200

    begin loops exercise js

---
+++ JS/loops.js
@@ -0,0 +1,13 @@
+/*First For Loop Practice
+
+Please use a for loop to print out the classic chorus lyrics of "I'm Blue" by Eiffel 65.  
+Use a for loop to print out the string "Da ba dee da ba daa" exactly 6 times.*/
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loopfor
+const i = 'Da ba dee da ba daa'
+for (let i = 1; i <= 6; i++) {
+    console.log('Da ba dee da ba daa');
+}
+
+
+
