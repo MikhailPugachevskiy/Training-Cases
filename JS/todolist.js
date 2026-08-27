a7c11a80b3537a508795698513b239fbe9a9cf04
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-27 18:14:50 +0200

    completed todolist project js

--- JS/todolist.js
+++ JS/todolist.js
@@ -12,8 +12,13 @@ while (input !== 'quit' && input !== 'q') {
         todos.push(newTodo);
         console.log(`${newTodo} added to the list!`)
     } else if (input === 'delete') {
-        const index = prompt('Ok, enter an index tp delete:');
-        console.log(index);
+        const index = parseInt(prompt('Ok, enter an index tp delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown index')
+        }
     }
     input = prompt('What would you like to do?')
 }
