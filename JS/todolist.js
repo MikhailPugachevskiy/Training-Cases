77f7ed162c99dd194bcb1994fa0efddc37bff6fe
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-27 17:38:26 +0200

    update todolist project js

--- JS/todolist.js
+++ JS/todolist.js
@@ -1,5 +1,20 @@
-let input = prompt("What would you like to do?")
+let input = prompt('What would you like to do?');
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
 while (input !== 'quit' && input !== 'q') {
-    input = prompt("What would you like to do?")
+    if (input === 'list') {
+        console.log('*****************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*****************')
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = prompt('Ok, enter an index tp delete:');
+        console.log(index);
+    }
+    input = prompt('What would you like to do?')
 }
-console.log("OK QUIT THE APP!")
+console.log('OK QUIT THE APP!')
