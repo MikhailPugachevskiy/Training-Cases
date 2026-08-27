f256e92cece677425f985fb0a8aa5c2b36615139
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-27 23:45:11 +0200

    begin function exercises js

---
+++ JS/functions.js
@@ -0,0 +1,16 @@
+/*Heart Function Exercise
+
+It's time to write our first function!
+
+    Define a function called printHeart that prints out the string "<3"
+
+    Execute your function once*/
+
+// Write your function here:
+function printHeart() {
+    console.log("<3");
+}
+printHeart();
+
+
+
