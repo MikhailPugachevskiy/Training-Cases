17ce269e64b664b596b7cb8cd2a55bffb4310d17
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-28 23:45:21 +0200

    update functions exercise js

--- JS/functions.js
+++ JS/functions.js
@@ -252,3 +252,21 @@ console.log(deadlyAnimal)
 
 "Scorpionfish"
 "Blue-Ringed Octopus"
+
+
+
+/*Function Expression Exercise
+
+Define a function that returns the square of a number. Define the function as a function expression, stored in a variable called square.  
+NOTE: Udemy's coding exercise platform does NOT support the ** operator. You'll need to multiply a number by itself or use the Math.pow() method.
+
+    square(4) //16
+    square(3) //9 */
+
+const square = function (num) {
+    return Math.pow(num, 2);
+}
+
+const square = function (num) {
+    return num * num;
+}
