6e567384f4d28d9dd4d4674be970beba7dd4a563
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-30 17:10:17 +0200

    update arrays exercise js

--- JS/arrays.js
+++ JS/arrays.js
@@ -93,3 +93,19 @@ const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', las
 const firstNames = fullNames.map(function (name) {
     return name.first
 });
+
+
+
+/*Arrow Function Exercise
+
+Write an arrow function expression called greet.  It should accept a single string argument, representing a person's name.  It should return a greeting string as shown below:
+
+greet("Hagrid") should return this string: Hey Hagrid! 
+
+greet("Luna") should return this string: Hey Luna!
+
+Be sure to use arrow function syntax!*/
+
+const greet = (name) => {
+    return `Hey ${name}!`;
+}
