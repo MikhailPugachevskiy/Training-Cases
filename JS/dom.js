b08fabe5f46f6be7f28228c115ef61ccf8675a08
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-02 23:53:32 +0200

    update dom exercises js

--- JS/dom.js
+++ JS/dom.js
@@ -68,3 +68,30 @@ Write your code in app.js.  Please do not change any of the elements in index.ht
 // Your code goes in here!
 const doneTodos = document.querySelectorAll('.done');
 const checkbox = document.querySelector('input[type="checkbox"]');
+
+
+
+/*Pickles Exercise
+
+Provided you with some basic markup in index.html
+
+    Please use JavaScript to select the <span> element that currently reads "Delicious"
+
+    Change its text to read "Disgusting" USING JAVASCRIPT. Please change the text to "Disgusting". 
+
+<!DOCTYPE html>
+
+<head>
+    <title>Pickles</title>
+    <!--LEAVE THESE LINES ALONE, PLEASE! THEY MAKE THE LIVE PREVIEW WORK!-->
+    <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+</head>
+
+<body>
+    <!--PLEASE LEAVE THIS LINE ALONE! MAKE YOUR CHANGES USING JAVASCRIPT!!-->
+    <h1>Pickles Are <span>Delicious</span></h1>
+</body>
+
+</html> */
