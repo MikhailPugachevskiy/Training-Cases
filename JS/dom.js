5f7f8fe99268a391dfa9491706faf3b371a5250a
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-03 16:48:12 +0200

    update dom exercise js

--- JS/dom.js
+++ JS/dom.js
@@ -128,7 +128,8 @@ eggImg.alt = 'chicken';
 
 /*Magical Forest Circle Exercise
 
-I've provided you with some basic markup in index.html.  Please do not change any of the markup directly.  Instead, use JavaScript to make the following changes:
+I've provided you with some basic markup in index.html.  Please do not change any of the markup directly.  
+Instead, use JavaScript to make the following changes:
 
     Select the div with the id of container.  Using JavaScript, set it's text alignment to 'center';
 
@@ -165,3 +166,46 @@ container.style.textAlign = 'center';
 
 
 
+/*Rainbow Text Exercise
+
+I've provided you with an <h1> element which contains 7 individual spans (each holding a single letter). 
+
+    Please write some JavaScript to make them rainbow-colored! 
+
+    In app.js you'll find an array of color names called colors.  It looks like: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].
+
+    Your task is to select all spans, iterate over them, and assign them each one of the colors from the colors array.  
+    The first span should be red, the second should be orange, etc.   Your result should look like this:
+
+<!DOCTYPE html>
+
+<head>
+    <title>Rainbow</title>
+    <!--LEAVE THESE LINES ALONE, PLEASE! THEY MAKE THE LIVE PREVIEW WORK!-->
+    <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/nodelist-foreach-polyfill@1.2.0/index.min.js"></script>
+</head>
+
+<body>
+    <!--DON'T TOUCH THIS FILE!-->
+    <h1>
+        <span>R</span>
+        <span>A</span>
+        <span>I</span>
+        <span>N</span>
+        <span>B</span>
+        <span>O</span>
+        <span>W</span>
+    </h1>
+</body>
+
+</html> */
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOUR CODE GOES HERE:
+const spans = document.querySelectorAll('span');
+for (let i = 0; i < spans.length; i++) {
+    spans[i].style.color = colors[i];
+}
