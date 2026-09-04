29bbed29bfc71b7b45626704b77b37bfd3b53311
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-04 23:46:45 +0200

    update domevents exercise js

--- JS/domevents.js
+++ JS/domevents.js
@@ -70,3 +70,68 @@ goodbyeButton.addEventListener('click', function () {
 
 
 
+/*Form Events Exercise
+
+index.html already has a form element that contains two <input> elements, one for quantity and one for a product name.  
+index.html also contains an empty <ul> where you will append new <li>'s.  Watch the gif at the bottom for an overview of 
+how your code should work. Your task is to follow these steps:
+
+    Listen for the form submission
+
+    When the form is submitted, prevent the default behavior
+
+    Grab the quantity input value and the product input value
+
+    Create a new <li> element.  Set the text on the new <li> to include the quantity and product name from the form.
+
+    Append the new <li> to the <ul> on the page
+
+    Reset the inputs
+
+Please note:
+The form will need to be assigned to a variable named form for the test to pass, 
+already included is this line of code for you in the app.js code.
+
+<!DOCTYPE html>
+
+<head>
+    <title>Grocery List</title>
+    <!--LEAVE THESE LINES ALONE! THEY MAKE THE LIVE PREVIEW WORK!-->
+    <script src="node_modules/babel-polyfill/dist/polyfill.js" type="text/javascript"> </script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+</head>
+
+<body>
+    <h1>Grocery List</h1>
+    <form action="/nowhere">
+        <label for="product">Enter A Product</label>
+        <input type="text" id="product" name="product">
+        <label for="qty">Enter A Quantity</label>
+        <input type="number" id="qty" name="qty">
+        <button>Submit</button>
+    </form>
+
+    <ul id="list"></ul>
+    
+    
+    
+    <!--LEAVE THE BELOW CODE ALONE TOO! ALSO, NO NEED TO STUDY THE BELOW CODE, 
+    IT'S JUST AN INNER MECHANISM TO MAKE THIS CODING EXERCISE WORK FULLY!-->
+    <script>(function(arr){arr.forEach(function(item){if(item.hasOwnProperty('append'))return;Object.defineProperty(item,'append',{configurable:true,enumerable:true,writable:true,value:function append(){var argArr=Array.prototype.slice.call(arguments),docFrag=document.createDocumentFragment();argArr.forEach(function(argItem){var isNode=argItem instanceof Node;docFrag.appendChild(isNode?argItem:document.createTextNode(String(argItem)));});this.appendChild(docFrag);}});});})([Element.prototype,Document.prototype,DocumentFragment.prototype]);</script>    
+</body>
+
+</html>*/
+
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
+const ul = document.querySelector('#list');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const qty = form.elements.qty.value;
+    const product = form.elements.product.value;
+    const li = document.createElement('li');
+    li.innerText = `${qty}${product}`;
+    ul.appendChild(li);
+    form.reset();
+});
