0e4b82c6b9e3d20183dfe08f3acd564f39c44f4b
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-05 21:09:04 +0200

    solve basics exercise ts

---
+++ TS/basics.ts
@@ -0,0 +1,9 @@
+/*let myName: string = "Bob"
+// Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
+
+let numberOfWheels = 4
+let isStudent = false
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false*/
