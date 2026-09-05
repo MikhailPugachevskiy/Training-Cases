ac17491b5b1ce3b1eca32fd8f1f91cca9de46398
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-05 21:59:04 +0200

    update basics exercise ts

--- TS/basics.ts
+++ TS/basics.ts
@@ -7,3 +7,29 @@ let isStudent = false
 
 let numberOfWheels: number = 4
 let isStudent: boolean = false*/
+
+
+
+//Defining Custom Types
+// Challenge: finish the object type definition
+
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
+
+
+
