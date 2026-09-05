2c4bf6a12602c13574be9c75f77450e70e364a91
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-05 17:39:55 +0200

    update pizza app exercise ts

--- TS/pizzaapp.ts
+++ TS/pizzaapp.ts
@@ -30,6 +30,10 @@ function addNewPizza(pizzaObj) {
 
 function placeOrder(pizzaName) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    if (!selectedPizza) {
+        console.error(`Pizza with name "${pizzaName}" does not exist in the menu.`)
+        return
+    }
     cashInRegister += selectedPizza.price
     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
