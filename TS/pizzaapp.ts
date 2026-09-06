113a1bf2fa99bafd25b0628745a156a4924432a2
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-06 20:52:34 +0200

    update pizza app exercise ts

--- TS/pizzaapp.ts
+++ TS/pizzaapp.ts
@@ -6,6 +6,7 @@ export { }
  */
 
 type Pizza = {
+    id: number
     name: string
     price: number
 }
@@ -26,11 +27,11 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ]
 
 /**
@@ -82,9 +83,9 @@ function completeOrder(orderId: number) {
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
