9de388955d447ef2ed00cdb25a6f3ab254dcfaf7
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-06 14:53:33 +0200

    solved exercise nested object types ts

--- TS/basics.ts
+++ TS/basics.ts
@@ -1,6 +1,6 @@
 /*let myName: string = "Bob"
 // Primitive Data Types: string, number, boolean
-// Challenge: Explicitly type the variables below:
+// Exercise 1: Explicitly type the variables below:
 
 let numberOfWheels = 4
 let isStudent = false
@@ -11,9 +11,9 @@ let isStudent: boolean = false*/
 
 
 //Defining Custom Types
-// Challenge: finish the object type definition
+// Exercise 2: finish the object type definition
 
-type Person = {
+/*type Person = {
     name: string
     age: number
     isStudent: boolean
@@ -30,6 +30,78 @@ let person2: Person = {
     age: 66,
     isStudent: false
 }
+*/
 
+/*
+//Exercise Nr. 3
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+    address: {
+        street: string
+        city: string
+        country: string
+    }
+}
 
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
 
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+*/
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+type Address = {
+    street: string
+    city: string
+    country: string
+}
+
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+    address: Address
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
