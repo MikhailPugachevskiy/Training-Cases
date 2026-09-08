8436f9a5494049deaa51afcb7a73ebeedf5615f9
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-08 16:12:03 +0200

    begin SQL exercise task 1 select sql

---
+++ SQL/query.sql
@@ -0,0 +1,8 @@
+/*SELECT all*/
+SELECT * FROM cars;
+
+/*Selecting colums
+	Exercise 1: Select the brand, model, condition and year from the cars table
+*/
+SELECT brand, model, condition, year FROM cars;
+
