8391e61cf284806b72db120cbf038b84d9bbf0be
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-08 17:00:50 +0200

    solved exercise 6 & 7 not equal sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -29,3 +29,14 @@ SELECT brand, model, condition, price FROM cars
 SELECT brand, model, condition, price FROM cars
 	WHERE price < 50000;
 
+/*Not equal
+Exercise 6: Filter out cars from 1965. Select the brand, model, year and price*/
+
+SELECT brand, model, year, price FROM cars
+	WHERE year != 1965;
+
+/*Exercise 7: Find cars which are not yellow. Select the brand, model, price and color*/
+
+SELECT brand, model, price, color FROM cars
+	WHERE color != 'yellow';
+
