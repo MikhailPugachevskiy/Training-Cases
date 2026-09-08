d6c51a93e45bff4a69fa74253adf4ee1b8b58634
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-08 16:29:21 +0200

    solving exercises 1 & 2, select & WHERE sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -2,7 +2,17 @@
 SELECT * FROM cars;
 
 /*Selecting colums
-	Exercise 1: Select the brand, model, condition and year from the cars table
-*/
+Exercise 1: Select the brand, model, condition and year from the cars table*/
 SELECT brand, model, condition, year FROM cars;
+
+/*WHERE Clause
+Exercise 2: Select the brand, model, color and price from cars where the color is black*/
+
+SELECT brand, model, color, price FROM cars
+	WHERE color = 'black';
+
+/*Exercise 3: Select the brand, model, condition and price from cars where the condition equals 0*/
+
+SELECT brand, model, condition, price FROM cars
+	WHERE condition = 0;
 
