71088f9a1b41f59d7a40397446e134758fe50651
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-09 23:54:26 +0200

    solved LIMIT exercises 25 & 26 sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -251,3 +251,31 @@ SELECT brand, model, condition, price FROM cars
 	WHERE sold IS FALSE
 	AND condition != 5
 	ORDER BY condition DESC, price;
+
+
+
+/*LIMIT
+
+Exercise 25:
+	Select the brand, model, year and price from the cars table
+		order the results by the price in descending order
+		limit the results to 1*/
+
+SELECT brand, model, year, price FROM cars
+	ORDER BY price DESC
+	LIMIT 1;
+
+
+
+/*Exercise 26:
+	Select the brand, model, color and price from cars
+		where the color is a shade of 'red'
+		and sold is false
+		order by price
+		limit the results to 5*/
+
+SELECT brand, model, color, price FROM cars
+	WHERE color LIKE '%red%'
+	AND sold IS FALSE
+	ORDER BY price
+	LIMIT 5;
