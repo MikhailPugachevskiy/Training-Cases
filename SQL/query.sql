e9a2fb45320a0c5524b60a380e726993024afed2
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-09 23:41:49 +0200

    solved exercises 19, 20, 21 sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -167,7 +167,8 @@ SELECT brand, model, condition, year FROM cars
 
 
 
-/*Exercise 18: Select brand, model, price and sold from cars 
+/*Exercise 18: 
+Select brand, model, price and sold from cars 
 		filter out any cars which are sold
 		show cars where the brand is none of ('Ford', 'Triumph', 'Chevrolet', 'Dodge')
 		or the price is less than $50000*/
@@ -180,5 +181,40 @@ SELECT brand, model, price, sold FROM cars
 
 
 
+/*Exercise 19:
+	Select brand, model, and color from cars
+		where the color is 'red'
+		and the brand is not 'Ferrari'
+		and the car has not been sold*/
+
+SELECT brand, model, color FROM cars
+  WHERE color LIKE '%red%'
+  AND brand != 'Ferrari'
+  AND sold IS FALSE;
+
+
+
+/*Exercise 20:
+	Select brand, model, and color from cars
+		where the color is not red, blue, or white
+		and the brand is none of: Aston Martin, Bentley or Jaguar
+		and sold is false*/
+
+SELECT brand, model, color FROM cars
+  WHERE color NOT IN ('red', 'blue', 'white')
+  AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
+  AND sold IS FALSE;
+
+
+/*Exercise 21:
+	Select brand, model, year, sold from cars
+		where the brand is 'Dodge' and year is in the 60s
+		or the brand is either 'Ford' or 'Triumph' and the car is from the 70s
+		only select cars where sold is not true*/
+
+SELECT brand, model, year, sold FROM cars
+  WHERE ((brand = 'Dodge' AND year BETWEEN 1960 AND 1969)
+  OR (brand IN ('Ford', 'Triumph') AND year BETWEEN 1970 AND 1979))
+  AND SOLD IS NOT TRUE;
 
 
