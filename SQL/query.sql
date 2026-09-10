8fb7358b1ebb80a34a824fc3c37e2de097b44e3f
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-10 20:13:03 +0200

    solved DELETE exercises 44 & 45 sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -529,3 +529,16 @@ WHERE
 
 
 
+/*DELETE
+
+Exercise 44: Delete from the cars table, any record where condition is 0*/
+
+DELETE FROM cars
+	WHERE condition = 0;
+
+
+
+/*Exercise 45: Delete any record from the cars table where sold is TRUE*/
+
+DELETE FROM cars
+	WHERE sold IS TRUE;
