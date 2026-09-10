c3aae7789ccf928828416819264060b135feb84d
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-10 16:01:11 +0200

    solved MAX. MIN, AVG exercises 29-31

--- SQL/query.sql
+++ SQL/query.sql
@@ -300,3 +300,41 @@ SELECT COUNT(*) FROM cars
 
 SELECT SUM(price) AS total_earnings FROM cars
 	WHERE sold IS TRUE;
+
+
+
+/*MAX, MIN and AVG
+
+Exercise 29:
+	Select the maximum retail price
+		where sold is true
+	Use most_expensive as an alias*/
+
+SELECT MAX(price) AS most_expensive FROM cars
+	WHERE sold IS TRUE;
+
+
+
+/*Exercise 30:
+	Use the AVG aggregate function to find the average price
+		where the brand is Bentley
+		We can use FLOOR and CEIL to round the average down or up
+			to the nearest whole number*/
+
+SELECT FLOOR(AVG(price)) AS average FROM cars
+	WHERE brand = 'Bentley';
+
+
+
+/*Exercise 31:
+	Select the average, minimum and maximum price from cars
+		where sold is true
+	Round the average up to the nearest whole number
+		and use 'avg' as the alias for that result	*/
+
+SELECT
+	CEIL(AVG(price)) AS avg,
+	MIN(price),
+	MAX(price)
+FROM cars
+	WHERE sold IS TRUE;
