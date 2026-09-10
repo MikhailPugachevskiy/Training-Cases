eaa0e3e3ab2dd2c0ecfde8e414f75b7db16a1630
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-10 16:45:51 +0200

    begonnen HelloWorld aufgabe java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main (String args[]) {
+        String str1, str2;
+        str1 = "Hello World!";
+        str2 = "My Java Program!";
+        System.out.printIn(str1 +""+str2);
+    }
+}
