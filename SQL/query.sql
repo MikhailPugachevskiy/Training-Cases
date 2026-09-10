ee46f55b9b9282dbfa4d7e3d78f1fe7f1a89c63d
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-10 20:01:36 +0200

    solved UPDATE exercises 41-43 sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -486,3 +486,46 @@ INSERT INTO cars (
 ), (
 	'Porsche', '944 Turbo', 1986, 48000, 'white', 4, FALSE
 );
+
+
+
+/*UPDATE
+
+Exercise 41:
+	Set the sold column to true for the Ford Escort RS2000*/
+
+UPDATE cars SET
+	sold = TRUE
+WHERE brand = 'Ford'
+	AND model = 'Escort RS2000';
+
+
+
+/*Exercise 42:
+	Update the record for the Aston Martin DB4 with ID 14
+		set the condition to 5
+		and the price to 465000*/
+
+UPDATE cars SET
+	condition = 5,
+	price = 465000
+WHERE 
+	id = 14;
+
+
+
+/*Exercise 43: 
+	Set the condition to 1
+		and the price to $10,000
+	where the car's brand is Porsche
+		and sold is false*/
+
+UPDATE cars SET
+	condition = 1,
+	price = 10000
+WHERE
+	brand = 'Porsche'
+	AND sold IS FALSE;
+
+
+
