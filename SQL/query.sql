0d9183417b60587ad65f4446f738353f1693b2fc
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-10 17:48:40 +0200

    solved GROUP BY exercises 32-34 sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -338,3 +338,45 @@ SELECT
 	MAX(price)
 FROM cars
 	WHERE sold IS TRUE;
+
+
+
+/*GROUP BY
+
+Exercise 32:
+	Select the brand, and a count of the brand from cars
+		alias the count as brand_count
+		group by the brand column*/
+
+SELECT brand, count(brand) AS brand_count FROM cars
+	GROUP BY brand;
+
+
+
+/*Exercise 33:
+	Select the condition, and a count of the condition from cars
+		group by the condition column*/
+
+SELECT condition, count(condition) FROM cars
+	GROUP BY condition;
+
+
+
+/*Exercise 34:
+	Select:
+		* the brand
+		* a count of the brand
+		* and an average of the price for each brand
+		* round the average down to the nearest number
+		* alias the average as 'AVG' in your output
+	From cars where
+		the car has not been sold
+	Group the table by brand.*/
+
+SELECT brand, count(brand), FLOOR(AVG(price)) AS AVG
+	FROM cars
+	WHERE sold IS FALSE
+	GROUP BY brand;
+
+
+
