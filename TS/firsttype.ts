4d579a6f2512840dcc78c2247f7eb3fc0f664af4
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-11 23:46:32 +0200

    bearbeitet firsttype annotation ts

---
+++ TS/firsttype.ts
@@ -0,0 +1,14 @@
+let x: number = 0;
+x = 1;
+console.log(typeof x);
+
+/*let x: boolean = false;
+x = true;
+
+console.log(typeof x);
+
+x = false;*/
+
+/*let x: string = "1";
+x = 1; Type 'number' is not assignable to type 'string'*/
+
