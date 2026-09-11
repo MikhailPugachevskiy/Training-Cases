0bed16afc3e6f75ee36df56697f13d1c91dd28fe
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-11 15:24:53 +0200

    prepared intro TS

---
+++ TS/intro.js
@@ -0,0 +1,20 @@
+//Wurde aus intro.ts umgewandelt
+
+/*"use strict";
+const contentElement = getContentElement();
+if (contentElement.type === "image") {
+    contentElement.src;
+}
+else {
+    contentElement.text;
+}
+function getContentElement() {
+    if (Math.random() >= 0.5) {
+        const returnObj = { type: "image", src: "https://..." };
+        return returnObj;
+    }
+    else {
+        const returnObj = { type: "text", text: "text" };
+        return returnObj;
+    }
+}*/
---
+++ TS/intro.ts
@@ -0,0 +1,28 @@
+type ContentImage = {
+    type: "image";
+    src: string;
+};
+
+type ContentText = {
+    type: "text";
+    text: string;
+};
+
+const contentElement = getContentElement();
+
+if (contentElement.type === "image") {
+    contentElement.src;
+} else {
+    contentElement.text;
+}
+
+
+function getContentElement(): ContentImage | ContentText {
+    if (Math.random() >= 0.5) {
+        const returnObj: ContentImage = { type: "image", src: "https://..." };
+        return returnObj;
+    } else {
+        const returnObj: ContentText = { type: "text", text: "text" };
+        return returnObj;
+    }
+}
