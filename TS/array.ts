0ca7ccd8bc6626558cce05598f30f8694f727035
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-12 23:03:27 +0200

    learned array & tupel array ts

---
+++ TS/array.ts
@@ -0,0 +1,19 @@
+//Array
+
+const fruits = ["orange", "apple", "banana", 1];
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+fruits.push(21);
+
+
+
+//Tupel array
+
+const vec2d: [number, number, string] = [0, 0, "Nullvektor"];
+
+vec2d[0] = 1;
+vec2d[1] = 2;
+vec2d[2] = "";
