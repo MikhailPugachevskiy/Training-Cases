6931e2b3dc207e30697dee252a6f069a6c965660
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-12 19:12:00 +0200

    learned type any ts

---
+++ TS/typeany.ts
@@ -0,0 +1,14 @@
+//Type Any -> avoid whenever possible
+let result = loopPrint(10, "", true);
+console.log(result);
+
+function loopPrint(count: number, text: string, postfix: boolean): number {
+    let z: any;
+    for (let i = 0; i < count; i++) {
+        postfix ? console.log(text + i) : console.log(i + text);
+        z = i;
+    }
+
+    z?.toFixed();
+    return 0;
+}
