39b4d4c558c0b2459f8e376c7f70794e7944b3fe
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-15 17:40:37 +0200

    update score keeper project js

--- JS/scorekeeper.js
+++ JS/scorekeeper.js
@@ -1,14 +1,33 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const resetButton = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const playToSelect = document.querySelector('#playto');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
 
-let p1Score = 0;
-let p2Score = 0;
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const playToSelect = document.querySelector('#playto');
 let winningScore = 3;
 let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
 
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
@@ -20,9 +39,10 @@ p1Button.addEventListener('click', function () {
             p1Button.disabled = true;
             p2Button.disabled = true;
         }
-        p1Display.textContent = p1Score;
+        p1Display.textContent = p2Score;
     }
 })
+
 
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
@@ -53,4 +73,6 @@ function reset() {
     p2Display.textContent = 0;
     p1Display.classList.remove('has-text-success', 'has-text-danger');
     p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
