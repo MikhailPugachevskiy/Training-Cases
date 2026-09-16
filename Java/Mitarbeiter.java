3efab6a89fd5508eb7bcbb5be452da1f1065b5a1
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-16 22:46:38 +0200

    fortgesetzt Kapitel 2 Objektorientierung + Mitarbeiter java

---
+++ Java/Mitarbeiter.java
@@ -0,0 +1,25 @@
+//Kapitel 2: Objektorientierung
+
+public class Mitarbeiter {
+	
+	//Instanzvariable
+	int personalnummer; //unique 
+	int arbeitsstunden; 
+	String vorname;
+	String nachname;
+	String anschrift;
+	boolean istExternerMitarbeiter; 
+	//Klassenvariable
+	static int anzahlMitarbeiter; 
+	
+	//Konstruktor ist eine spezielle Methode
+	Mitarbeiter(int p, int a, String v, String n, String addr, boolean iEM){
+	 this.personalnummer = p; 	
+	 this.arbeitsstunden = a;
+	 this.vorname = v;
+	 this.nachname = n;
+	 this.anschrift = addr;
+	 this.istExternerMitarbeiter = iEM;
+	}
+
+}
