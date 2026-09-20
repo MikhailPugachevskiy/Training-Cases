e0e7c96bb5a6b41dc416a82e885c6b2e538fad14
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-09-20 17:20:16 +0200

    update exercise codesandbox component decomposition react

---
+++ React/CodeSandbox/src/DiceRoll.jsx
@@ -0,0 +1,11 @@
+import Die from "./Die";
+export default function DiceRoll() {
+    return (
+        <div>
+            <h1>Dice Roll</h1>
+            <Die />
+            <Die />
+            <Die />
+        </div>
+    )
+}
